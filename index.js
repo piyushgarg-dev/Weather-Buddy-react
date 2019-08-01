dcdd3e9fcf660817a3855e066448abf3be652047
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-08-01 20:13:58 +0530

    update

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "production") {
+
   app.use(express.static("build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
-}
+
 
 app.listen(PORT, () => {
   console.log(`Server Started at ${PORT}...`);
