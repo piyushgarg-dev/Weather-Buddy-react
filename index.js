29a17d88a769789645195f1206206262983cede5
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-08-02 20:55:36 +0530

    Server Build

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-  app.use(express.static("build"));
+const PORT = process.env.PORT || 3002;
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
+app.use(express.static('client/build'));
 
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 
-app.listen(PORT, () => {
-  console.log(`Server Started at ${PORT}...`);
-});
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server at: ${PORT}`);
+})
